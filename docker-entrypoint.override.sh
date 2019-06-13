35550f3a74fd9283c0164760be44439122be55da
francesco <info.francescobianco@gmail.com>
2019-06-13 11:13:13 +0200

    first commit

--- docker-entrypoint.override.sh
+++ docker-entrypoint.override.sh
@@ -4,7 +4,7 @@ set -e
 WRITABLE=/tmp/$RANDOM
 
 if [[ $(touch ${WRITABLE}) ]]; then
-    echo ">>> Error: Mount writable volume on '/tmp' for user:group '$(id -u):$(id -g)'."
+    echo ">>> Error: Volume '/tmp' for user:group '$(id -u):$(id -g)'."
     exit 1
 fi
 
